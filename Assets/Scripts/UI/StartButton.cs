955c7750137e887287eb106149052901c4b560aa
oneagustiranda <56112959+oneagustiranda@users.noreply.github.com>
2021-06-21 00:20:58 +0700

    add fix marker, clean button, and scale rotate object

--- Assets/Scripts/UI/StartButton.cs
+++ Assets/Scripts/UI/StartButton.cs
@@ -5,10 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
+    public string sceneName;
     // Start is called before the first frame update
     public void Mulai()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Exit()
+    {
+        Debug.Log("exitApps");
+        Application.Quit();
+    }
 }

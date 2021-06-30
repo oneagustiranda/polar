ba10f6aea0b5d6c18de039b6f987e738df5c7efa
oneagustiranda <56112959+oneagustiranda@users.noreply.github.com>
2021-06-30 20:37:49 +0700

    fix maps ui 3d polibatam

--- Assets/Scripts/Animator/BuildingAnimator.cs
+++ Assets/Scripts/Animator/BuildingAnimator.cs
@@ -6,11 +6,11 @@ public class BuildingAnimator : MonoBehaviour
 {
     public void Show()
     {
-        LeanTween.moveLocalY(gameObject, 0f, 0.5f).setEaseInExpo();
+        LeanTween.moveLocalY(gameObject, -0.014f, 0.5f).setEaseInExpo();
     }
 
     public void Hide()
     {
-        LeanTween.moveLocalY(gameObject, -1.642313f, 0.5f).setEaseInExpo();
+        LeanTween.moveLocalY(gameObject, -1.65f, 0.5f).setEaseInExpo();
     }
 }
